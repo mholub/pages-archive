0e716e1d6849912b3466374ed50b322135d0dc8b
Michael Holub <michael.holub@gmail.com>
2015-07-23 04:03:06 +0300

    preset 3

--- lissajous/js/LissajousThree.js
+++ lissajous/js/LissajousThree.js
@@ -2,6 +2,10 @@ function circlePoint(radius, nangle) { // angle is [0, 1]
   return new THREE.Vector3(radius * Math.cos(nangle * 2 * Math.PI), radius * Math.sin(nangle * 2 * Math.PI), 0);
 }
 
+function circlePoint2(radius, xangle, yangle) { // angle is [0, 1]
+  return new THREE.Vector3(radius * Math.cos(xangle * 2 * Math.PI), radius * Math.sin(yangle * 2 * Math.PI), 0);
+}
+
 function updatePoints(geometry, data) {
   for ( i = 0; i < geometry.vertices.length; i ++ ) {
     var t = i / (geometry.vertices.length - 1);
@@ -36,9 +40,9 @@ function Figure1Data() {
     var pointNT = nt * this.primPointsCount - primPointNum;
 
     var prevPointInPrim = circlePoint(this.primRadius, (primPointNum + k) / this.primPointsCount
-     + (this.orientPrims ? primPhase : 0) + this.primPhaseOffset);
+     + (this.orientPrims ? primPhase + 0.5 : 0) + this.primPhaseOffset);
     var nextPointInPrim = circlePoint(this.primRadius, (primPointNum + 1 + k) / this.primPointsCount
-    + (this.orientPrims ? primPhase : 0) + this.  primPhaseOffset);
+    + (this.orientPrims ? primPhase + 0.5 : 0) + this.  primPhaseOffset);
 
     if (this.distortCorners) {
       if (pointNT <= 1 / this.primPointsCount) {
@@ -60,6 +64,56 @@ function Figure1Data() {
     this.phaseOffset = 0.1 * time;
     this.radius = Math.sin(0.3 * time);
     this.primRadius = 0.2 + 0.6 * Math.cos(0.1 * time);
+  }
+}
+
+function Figure3Data() {
+  this.primCount = 3; // how many shapes (ex: triangles)
+  this.radius = 1; // main circle radius (which connects centers of shapes)
+  this.phaseOffset = 0;
+  this.primPointsCount = 100; // how many points in shape (3 for triangle)
+  this.primRadius = 0.15;
+  this.primPhaseOffset = 0.0;
+  this.orientPrims = true;
+  this.distortCorners = true;
+
+  this.getPosition = function(t) {
+    var primNum = Math.floor(t * this.primCount); // index of figure (triangle, pentagon, etc) [0, primCount]
+    var nt = t * this.primCount - primNum; // normalized t inside the figure [0, 1]
+
+    var primPhase = primNum / this.primCount + this.phaseOffset;
+    var centerOfPrim =  circlePoint(this.radius, primPhase);
+
+    var primPointNum = Math.floor(nt * this.primPointsCount);
+
+    var quaternion = new THREE.Quaternion();
+    quaternion.setFromAxisAngle( centerOfPrim, -Math.PI/2 + 2 * Math.PI * this.phaseOffset );
+
+    var quaternion2 = new THREE.Quaternion();
+    quaternion2.setFromAxisAngle( new THREE.Vector3(0, 0, 1), -Math.PI/2 );
+
+    nt += this.primPhaseOffset;
+
+    var pointInPrim = circlePoint(this.primRadius * Math.cos(this.primPointsCount * nt) + 0.1 * Math.cos(this.phaseOffset), nt);
+    pointInPrim.setZ(0.8 * Math.cos(this.primPointsCount * nt));
+    pointInPrim.applyQuaternion(quaternion);
+    pointInPrim.applyQuaternion(quaternion2);
+
+    var p = new THREE.Vector3();
+    p.addVectors(centerOfPrim, pointInPrim);
+    return p;
+  }
+  this.animate = function(time) {
+
+    this.primPhaseOffset = -0.15;
+    this.phaseOffset = 0.1 * time;
+    this.radius = Math.sin(0.3 * time);
+    this.primRadius = 0.2 + 0.6 * Math.cos(0.1 * time);
+    var primPointCounts = [3, 4, 5, 8, 10];
+    var period = Math.ceil(time / 3) % primPointCounts.length;
+
+    this.primCount = primPointCounts[period];
+    this.primPointsCount = 10 * primPointCounts[period];
   }
 }
 
@@ -91,3 +145,5 @@ LissajousNgons2.animate = function(time) {
   this.primPointsCount = primPointCounts[period];
   this.primCount = 2 * primPointCounts[period];
 }
+
+LissajousNgons3 = new Figure3Data();
